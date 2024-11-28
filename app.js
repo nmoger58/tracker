be7fac99b7689ad9d2aaaa2109657a17b3c1c908
nmoger58 <nmoger58@gmail.com>
2024-11-28 23:28:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();
+const http=require('http');
+const socket=require('socket.io')
+const server=http.createServer(app);
+const path=require('path')
+const io=socket(server)
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"public")))
+
+io.on('connection',(socket)=>{
+    console.log("connected")
+    socket.on('send-location',(data)=>{
+     io.emit('recieve-location',{id : socket.id,...data})
+    })
+    socket.on('disconnect',()=>{
+        io.emit('user-disconnected',socket.id);
+    })
+})
+app.get('/',(req,res)=>{
+  res.render("index")
+})
+
+server.listen(3000)
---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+const socket=io('http://localhost:3000');
+
+// console.log("hey")
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+    (position)=>{
+       const {latitude,longitude}= position.coords;
+       socket.emit('send-location',{latitude,longitude})
+    },
+    (err)=>{
+        console.error(err)
+    },
+    {
+        enableHighAccuracy :true,
+        maximumAge : 0,//not to depend on old data use new data as soon as available
+        timeout : 5000, // after every 5 secs location is got
+    })
+}
+
+const map= L.map("map").setView([0,0],16)
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution :  "Striker Master Ice"
+}).addTo(map)
+
+const markers={};
+
+socket.on('recieve-location',(data)=>{
+    const {id,latitude,longitude}=data;
+    map.setView([latitude,longitude],16);// 20 is the zoom rate
+    if (markers[id]) {
+        markers[id].setLatLng([latitude,longitude])
+    }
+    else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map)
+    }
+})
+
+socket.on('user-disconnected',(id)=>{
+if(markers[id]){
+    map.removeLayer(markers[id]);
+    delete markers[id]
+}
+})
